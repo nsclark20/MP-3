f7768847908a43cdb54295164ee2805ca637946d
nsclark20 <nscl223@uky.edu>
2020-04-14 20:36:12 -0400

    Q4

--- jenkins/core/src/main/java/jenkins/model/IdStrategy.java
+++ jenkins/core/src/main/java/jenkins/model/IdStrategy.java
@@ -231,6 +231,7 @@ public abstract class IdStrategy extends AbstractDescribableImpl<IdStrategy> imp
             } else {
                 StringBuilder buf = new StringBuilder(filename.length());
                 final char[] chars = filename.toCharArray();
+                outterloop:
                 for (int i = 0; i < chars.length; i++) {
                     char c = chars[i];
                     if ('a' <= c && c <= 'z') {
@@ -247,29 +248,42 @@ public abstract class IdStrategy extends AbstractDescribableImpl<IdStrategy> imp
                     } else if (c == '$') {
                         StringBuilder hex = new StringBuilder(4);
                         i++;
-                        if (i < chars.length) {
-                            hex.append(chars[i]);
-                        } else {
-                            break;
-                        }
-                        i++;
-                        if (i < chars.length) {
-                            hex.append(chars[i]);
-                        } else {
-                            break;
-                        }
-                        i++;
-                        if (i < chars.length) {
-                            hex.append(chars[i]);
-                        } else {
-                            break;
-                        }
-                        i++;
-                        if (i < chars.length) {
-                            hex.append(chars[i]);
-                        } else {
-                            break;
-                        }
+			for(int j = 0; j < 4; j++){
+			    if(i < chars.length){
+			       hex.append(chars[i]);
+			    }else{
+			       break outterloop;
+			    }
+			    i++;	    
+			}
+		
+                        //if (i < chars.length) {
+                          //  hex.append(chars[i]);
+                       // } else {
+                       //     break;
+                       // }
+                       // i++;
+                       // if (i < chars.length) {
+                       //     hex.append(chars[i]);
+                       // } else {
+                       //     break;
+                       // }
+                       // i++;
+                       // if (i < chars.length) {
+                       //     hex.append(chars[i]);
+                       // } else {
+                       //     break;
+                       // }
+                       // i++;
+                       // if (i < chars.length) {
+                       //     hex.append(chars[i]);
+                       // } else {
+                       //     break;
+                       // }
+			
+			
+
+
                         buf.append(Character.valueOf((char)Integer.parseInt(hex.toString(), 16)));
                     }
                 }
